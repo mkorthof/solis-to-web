596edb72fe795658691f56a57914ab8e82849f96
Marius <mkorthof@users.noreply.github.com>
2026-09-10 22:57:06 +0200

    main.py: add rnd client_id

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import config
 import helper
 import web
-import plot
+import db
+from message import inverter_ts as message_ts
 
-from shlex import join
 import paho.mqtt.client as mqtt
 from paho.mqtt.client import CallbackAPIVersion
 import sys
 import os
+import random
 
 
 ################################
-''' Solis MQTT to Web Client '''
+''' Solis (MQTT) to Web Client '''
 ################################
 
 
@@ -20,42 +21,56 @@ def on_connect(client, userdata, connect_flags, reason_code, properties=None):
     client.subscribe(config.TOPIC)
 
 
-def write_period_html():
-    ''' Write message history and plot html files '''
-    for period in ['today'] + config.SHOW_PERIODS +  config.LINK_PERIODS:
-        with open(f'{config.WEBDIR}/messages_{period}.html', 'w', encoding='utf-8', errors='ignore') as f:
-            f.write(web.history_html(period))
-        plot.plot(period)
+def on_disconnect(client, userdata, disconnect_flags, reason_code, properties):
+    print(f'INFO: Disconnected with reason code: {reason_code}')
+
 
 def on_message(client, userdata, message):
     ''' Write payload from last message to json file, and create index.html '''
-    if not os.path.isdir(f'{config.WEBDIR}'):
-        os.mkdir(f'{config.WEBDIR}')            
-    if not os.path.isdir(f'{config.WEBDIR}/json'):
-        os.mkdir(f'{config.WEBDIR}/json')    
     payload = message.payload.decode()
     print(f'{helper.fmt_date()} INFO: Received {payload}')
-    with open(f'{config.WEBDIR}/json/{helper.prefix_date('now')}-solis.json', 'w', encoding='utf-8', errors='ignore') as f_json:
+    helper.webdirs()
+    helper.write_data()
+    # TODO: mkdirs for new fn format with path  e.g. ./y/m/d/<ts.json>
+    #os.makedirs(f'{helper.fn_json()}', exist_ok=True)
+    #print(f'DEBUG: on_message {helper._fpath_date('today')}/{message_ts(payload)}.json')
+    #with open(f'{config.WEBDIR}/json/{helper.fpath_date('today')}/{message_ts(payload)}.json', 'w', encoding='utf-8', errors='ignore') as f_json:
+    with open(f'{config.WEBDIR}/json/{helper.fn_now()}', 'w', encoding='utf-8', errors='ignore') as f_json:
         f_json.write(payload)
-    write_period_html()
     with open(f'{config.WEBDIR}/index.html', 'w', encoding='utf-8', errors='ignore') as f_index:
         f_index.write(web.index_html(payload))
 
 
 def test(period):
-    fn = f'{config.WEBDIR}/json/{helper.get_last_message(period)['pf']}-solis.json'
+    fn = f'{config.WEBDIR}/json/{helper.last_message(period)['pf']}-solis.json'
+    # fn = f'export/20260708_180444-solis.json'
     print(f'TEST: using "{fn}"')
     with open(f'{fn}', 'r', encoding='utf-8', errors='ignore') as f_json:
         payload = f_json.read()
-    write_period_html()
+    # TODO: test new fn format with path
+    #os.makedirs(helper._fpath_date('today'), exist_ok=True)
+    print(f'TEST: fmt_timestamp message_ts {helper.fmt_timestamp(message_ts(payload))} offset {helper.fmt_timestamp(message_ts(payload), True)}')
+    print(f'TEST: fmt_datetime message_ts {helper.fmt_datetime(message_ts(payload))} offset {helper.fmt_datetime(message_ts(payload), True)}')
+    print(f'TEST: new - makedirs {helper._fpath_date('today')}')  
+    print(f'TEST: new - fpath_date today "{config.WEBDIR}/json/{helper._fpath_date('today')}/{message_ts(payload)}.json"')
+    print(f'TEST: new - fpath_date yesterday "{config.WEBDIR}/json/{helper._fpath_date('yesterday')}/{message_ts(payload)}.json"')
+    print(helper.conv_fn())
+    exit(0)
+    helper.webdirs()
+    helper.write_data()
     with open(f'{config.WEBDIR}/index.html', 'w', encoding='utf-8', errors='ignore') as f_index:
         f_index.write(web.index_html(payload))
+    #db.insert(helper.db_date('now'), payload)
+    #db.insert(ts(payload), payload)
+
 
 def main():
     # v1: client = mqtt.Client()
-    client = mqtt.Client(CallbackAPIVersion.VERSION2, client_id="solis-to-web-client2")
+    client_id = f'solis-to-web-client-{random.randint(0, 1000)}'  # NOSONAR
+    client = mqtt.Client(CallbackAPIVersion.VERSION2, client_id=client_id)
     if config.BROKER_USERNAME and config.BROKER_PW:
         client.username_pw_set(config.BROKER_USERNAME, config.BROKER_PW)
+        #client.reconnect_delay_set(min_delay=1, max_delay=60)
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(config.BROKER_HOST, config.BROKER_PORT, 60)
@@ -63,6 +78,7 @@ def main():
     for item in dir(config):
         if not item.startswith("__") and os.environ.get(item):
             config.item = os.environ[item]
+
 
 if '--test' in sys.argv:
     try:
